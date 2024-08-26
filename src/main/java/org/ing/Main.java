a58eb8676bd171c8dd32e7563220792c6f204d54
Esi4 <kolupaeff13102001@yandex.ru>
2024-08-26 19:28:28 +0300

    test

--- src/main/java/org/ing/Main.java
+++ src/main/java/org/ing/Main.java
@@ -1,7 +1,10 @@
 package org.ing;
 
 public class Main {
+    public static String pathToFile = "";
+
     public static void main(String[] args) {
+        ReadInputFile readInputFile = new ReadInputFile(pathToFile);
         System.out.println("Hello world!");
     }
 }
--- src/main/java/org/ing/ReadInputFile.java
+++ src/main/java/org/ing/ReadInputFile.java
@@ -1,9 +1,18 @@
 package org.ing;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ReadInputFile {
-    String pathToFile;
+    private String pathToFile;
+    private HashMap<String, int[]> hashMap = new HashMap<>();
+    private File file;
 
     public ReadInputFile(String pathToFile) {
         if(!isValidFilePath(pathToFile)) {throw new IllegalArgumentException();}
@@ -11,16 +20,30 @@ public class ReadInputFile {
         this.pathToFile = pathToFile;
     }
 
-    public void readInputFile(String pathToFile) {
-
-    }
-
-    public static boolean isValidFilePath(String filePath) {
+    public boolean isValidFilePath(String filePath) {
         if (filePath == null || filePath.trim().isEmpty()) {
             return false;
         }
 
-        File file = new File(filePath);
+        file = new File(filePath);
         return file.exists() && file.isFile() && file.canRead();
     }
+
+    private void readFile() {
+        ArrayList<int[]> arr = new ArrayList<>();
+        BufferedReader bf;
+
+        try {
+            bf = new BufferedReader(new FileReader(pathToFile));
+            //TODO()
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void main() {}
+
+
 }
